74f500e225cce811df0108e2ecee6922bccd476a
Dorine B <Dorine@Oengus.local>
2014-12-16 14:40:22 +0100

    Connexion et déconnexion. In scription user et logement. Affichage logement.

--- Vue/meslog.php
+++ Vue/meslog.php
@@ -2,8 +2,13 @@
     if (empty($sousmenucompte)){
         $entete = entete("Mon site / Mes Logements");
         $menu = menu("moncompte");
-        $sousmenu = sousmenucompte("meslog");
-        $contenu = "<h2>Mes logements</h2><br /><a href='index.php?cible=inscrlog'>Ajouter un nouveau logement</a>";
+        if (isset($_SESSION["iduser"])){
+            $sousmenu = sousmenucompte("meslog");
+            $contenu = "<h1>Mes logements</h1><br/><a href='index.php?cible=inscrlog'>Ajouter un nouveau logement</a><br/><br/>";
+            $contenu .= afficherlog($_SESSION["iduser"]);
+        } else {
+            $contenu= "Vous n'êtes pas connectés ! Connectez vous avant.";
+        }
         $pied = pied();
     }
 
--- Vue/mesmess.php
+++ Vue/mesmess.php
@@ -2,8 +2,12 @@
     if (empty($sousmenucompte)){
         $entete = entete("Mon site / Mes Messages");
         $menu = menu("moncompte");
-        $sousmenu = sousmenucompte("mesmess");
-        $contenu = "<h2>Contenu de mes messages</h2>";
+        if (isset($_SESSION["iduser"])){
+            $sousmenu = sousmenucompte("mesmess");
+            $contenu = "<h1>Ma messagerie</h1>";
+        } else {
+            $contenu= "Vous n'êtes pas connectés ! Connectez vous avant.";
+        }
         $pied = pied();
     }
 
